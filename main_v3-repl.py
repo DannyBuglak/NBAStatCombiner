2a94f5ada04595820fcefb7c0713038a0a9feebb
Danny Buglak <dannybuglak4@gmail.com>
2025-11-16 11:28:59 -0500

    update season

--- main_v3-repl.py
+++ main_v3-repl.py
@@ -44,7 +44,7 @@ print("\n -> Player Selected: " + playerName + " <- ")
 # Find the player id
 playerId = data.players[playerIndex][0]
 
-season = "2025-26"
+season = "2025"
 
 pd.set_option('display.max_columns', None)          # Have all cols be available
 pd.set_option('display.max_rows', None)             # Have all rows be available
